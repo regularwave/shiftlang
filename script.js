c34c1f4970a5344a7217e4344c43d05ae9c5de1e
regularwave <regularwave@gmail.com>
2025-03-16 13:24:43 -0400

    split consonants-y and vowels+y

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 function stringBuilder(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
     return Array.from(str).map((char, index) => {
         const charCode = char.charCodeAt(0);
         if (index % 2 === 0) {
-            if (charCode >= 65 && charCode <= 90) {
-                return charCode === 90 ? 'A' : String.fromCharCode(charCode + 1);
-            } else if (charCode >= 97 && charCode <= 122) {
-                return charCode === 122 ? 'a' : String.fromCharCode(charCode + 1);
+            if (vowels.includes(char)) {
+                if (char === 'y') {
+                    return 'a';
+                } else if (char === 'Y') {
+                    return 'A';
+                } else {
+                    const vowelIndex = vowels.indexOf(char);
+                    return vowels[vowelIndex + 1];
+                }
+            } else if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+                return charCode === 90 ? 'A' : charCode === 122 ? 'a' : String.fromCharCode(charCode + 1);
             } else if (charCode >= 48 && charCode <= 57) {
                 return charCode === 57 ? '0' : String.fromCharCode(charCode + 1);
             } else {
                 return char;
             }
         } else {
-            if (charCode >= 65 && charCode <= 90) {
-                return charCode === 65 ? 'Z' : String.fromCharCode(charCode - 1);
-            } else if (charCode >= 97 && charCode <= 122) {
-                return charCode === 97 ? 'z' : String.fromCharCode(charCode - 1);
+            if (vowels.includes(char)) {
+                if (char === 'a') {
+                    return 'y';
+                } else if (char === 'A') {
+                    return 'Y';
+                } else {
+                    const vowelIndex = vowels.indexOf(char);
+                    return vowels[vowelIndex - 1];
+                }
+            } else if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+                return charCode === 65 ? 'Z' : charCode === 97 ? 'z' : String.fromCharCode(charCode - 1);
             } else if (charCode >= 48 && charCode <= 57) {
                 return charCode === 48 ? '9' : String.fromCharCode(charCode - 1);
             } else {
                 return char;
             }
         }
-    })
-        .join('');
+    }).join('');
 }
 
 function doConvert() {
