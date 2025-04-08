69c32d67e882f573190b72186b388da7887083f9
regularwave <regularwave@gmail.com>
2025-04-07 21:28:09 -0400

    array fixes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function stringBuilder(str) {
     const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+    const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'];
     return Array.from(str).map((char, index) => {
         const charCode = char.charCodeAt(0);
         if (index % 2 === 0) {
@@ -12,10 +13,15 @@ function stringBuilder(str) {
                     const vowelIndex = vowels.indexOf(char);
                     return vowels[vowelIndex + 1];
                 }
-            } else if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
-                return charCode === 90 ? 'A' : charCode === 122 ? 'a' : String.fromCharCode(charCode + 1);
-            } else if (charCode >= 48 && charCode <= 57) {
-                return charCode === 57 ? '0' : String.fromCharCode(charCode + 1);
+            } else if (consonants.includes(char)) {
+                if (char === 'z') {
+                    return 'b';
+                } else if (char === 'Z') {
+                    return 'B';
+                } else {
+                    const consonantIndex = consonants.indexOf(char);
+                    return consonants[consonantIndex + 1];
+                }
             } else {
                 return char;
             }
@@ -29,10 +35,15 @@ function stringBuilder(str) {
                     const vowelIndex = vowels.indexOf(char);
                     return vowels[vowelIndex - 1];
                 }
-            } else if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
-                return charCode === 65 ? 'Z' : charCode === 97 ? 'z' : String.fromCharCode(charCode - 1);
-            } else if (charCode >= 48 && charCode <= 57) {
-                return charCode === 48 ? '9' : String.fromCharCode(charCode - 1);
+            } else if (consonants.includes(char)) {
+                if (char === 'b') {
+                    return 'z';
+                } else if (char === 'B') {
+                    return 'Z';
+                } else {
+                    const consonantIndex = consonants.indexOf(char);
+                    return consonants[consonantIndex - 1];
+                }
             } else {
                 return char;
             }
